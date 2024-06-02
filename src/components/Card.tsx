2329e9703efc337fe735a279d9ea28a9470e638d
Nachiketha237 <nachiketha.cs21@bmsce.ac.in>
2024-06-02 11:38:36 +0530

    setuped Chakra Ui

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
+const MyCard = () => {
+    return (
+        <Card>
+            <CardHeader>Header</CardHeader>
+            <CardBody>Body</CardBody>
+            <CardFooter>Footer</CardFooter>
+        </Card>
+    );
+};
+export default MyCard;
